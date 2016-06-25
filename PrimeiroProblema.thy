28b509611e441e84d54466dd405ca96790f56252
Bruno Costa <brunoccst@gmail.com>
2016-06-25 19:26:13 -0300

    Primeiro commit

---
+++ PrimeiroProblema.thy
@@ -0,0 +1,17 @@
+theory PrimeiroProblema
+imports Main
+begin
+
+primrec multacc:: "nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat"
+      where
+        multacc01: "multacc 0 n a = a" |
+        multacc02: "multacc (Suc(k)) n a = multacc k n (a+n)"
+        
+fun mult:: "nat \<Rightarrow> nat \<Rightarrow> nat"
+  where
+    mult01: "mult n 0 = multacc 0 n 0" |
+    mult02: "mult n m = multacc n m 0"
+
+theorem multAcc:"\<forall>x y. multacc(x y z) = x * y + z"
+
+end
